b78117b1405cc98eb947174a70a7acbb1ae26d0e
Jon Penwood <jonp@polarity.io>
2020-12-23 14:16:51 -0600

    Added Limited Results Bug Fix

--- src/getLookupResults.js
+++ src/getLookupResults.js
@@ -4,84 +4,72 @@ const { partitionFlatMap, splitOutIgnoredIps } = require('./dataTransformations'
 const createLookupResults = require('./createLookupResults');
 const MAX_EVENTS_PER_ATTRIBUTE = 10;
 
-const getLookupResults = (
-  entities,
-  options,
-  requestWithDefaults,
-  Logger
-) =>
-  partitionFlatMap(
-    async (_entitiesPartition) => {
-      const { entitiesPartition, ignoredIpLookupResults } = splitOutIgnoredIps(
-        _entitiesPartition
-      );
-      const entitiesThatExistInMISP = fp.flatten(
-        await Promise.all(
-          fp.map(async (entity) => {
-            const searchResponse = await requestWithDefaults({
-              url: `${options.url}/attributes/restSearch`,
-              method: 'POST',
-              headers: {
-                Authorization: options.apiKey,
-                Accept: 'application/json',
-                'Content-type': 'application/json'
-              },
-              body: JSON.stringify({
-                value: entity.value.toLowerCase(),
-                limit: MAX_EVENTS_PER_ATTRIBUTE,
-                includeEventTags: true
-              })
-            });
-
-            return fp.flow(
-              fp.getOr([], 'body.response.Attribute'),
-              fp.map((attribute) => ({ ...attribute, type: entity.type }))
-            )(searchResponse);
-          }, entitiesPartition)
-        )
-      );
-      
-      const polarityTag = fp.get(
-        'body.Tag[0]',
-        await requestWithDefaults({
-          url: `${options.url}/tags/index/searchall:Submitted_By_Polarity`,
-          method: 'GET',
+const getLookupResults = async (entities, options, requestWithDefaults, Logger) => {
+  const { entitiesPartition, ignoredIpLookupResults } = splitOutIgnoredIps(entities);
+  const entitiesThatExistInMISP = fp.flatten(
+    await Promise.all(
+      fp.map(async (entity) => {
+        const searchResponse = await requestWithDefaults({
+          url: `${options.url}/attributes/restSearch`,
+          method: 'POST',
           headers: {
             Authorization: options.apiKey,
             Accept: 'application/json',
             'Content-type': 'application/json'
-          }
-        })
-      );
+          },
+          body: JSON.stringify({
+            value: entity.value.toLowerCase(),
+            limit: MAX_EVENTS_PER_ATTRIBUTE,
+            includeEventTags: true
+          })
+        });
 
-      const categoriesAndTypes = fp.get(
-        'body.result',
-        await requestWithDefaults({
-          url: `${options.url}/attributes/describeTypes`,
-          method: 'GET',
-          headers: {
-            Authorization: options.apiKey,
-            Accept: 'application/json',
-            'Content-type': 'application/json'
-          }
-        })
-      );
-
-      const lookupResults = createLookupResults(
-        options,
-        entitiesPartition,
-        entitiesThatExistInMISP,
-        polarityTag,
-        categoriesAndTypes
-      );
-
-      Logger.trace({ lookupResults, entitiesThatExistInMISP }, 'Lookup Results');
-
-      return lookupResults.concat(ignoredIpLookupResults);
-    },
-    20,
-    entities
+        return fp.flow(
+          fp.getOr([], 'body.response.Attribute'),
+          fp.map((attribute) => ({ ...attribute, type: entity.type }))
+        )(searchResponse);
+      }, entitiesPartition)
+    )
   );
+
+  const polarityTag = fp.get(
+    'body.Tag[0]',
+    await requestWithDefaults({
+      url: `${options.url}/tags/index/searchall:Submitted_By_Polarity`,
+      method: 'GET',
+      headers: {
+        Authorization: options.apiKey,
+        Accept: 'application/json',
+        'Content-type': 'application/json'
+      }
+    })
+  );
+
+  const categoriesAndTypes = fp.get(
+    'body.result',
+    await requestWithDefaults({
+      url: `${options.url}/attributes/describeTypes`,
+      method: 'GET',
+      headers: {
+        Authorization: options.apiKey,
+        Accept: 'application/json',
+        'Content-type': 'application/json'
+      }
+    })
+  );
+
+  const lookupResults = createLookupResults(
+    options,
+    entitiesPartition,
+    entitiesThatExistInMISP,
+    polarityTag,
+    categoriesAndTypes
+  );
+
+  Logger.trace({ lookupResults, entitiesThatExistInMISP }, 'Lookup Results');
+
+  return lookupResults.concat(ignoredIpLookupResults);
+};
 
 module.exports = {
   getLookupResults
