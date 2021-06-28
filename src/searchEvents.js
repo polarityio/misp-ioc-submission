b65ed481757315f9ff0d7f4bc96558c58bbabf4c
Jon Penwood <jonp@polarity.io>
2021-06-28 15:51:29 -0500

    Added Limit to Events Search

--- src/searchEvents.js
+++ src/searchEvents.js
@@ -1,5 +1,7 @@
 const fp = require('lodash/fp');
 
+const EVENTS_RETURN_LIMIT = 20;
+
 const searchEvents = async (
   { term, selectedEvent },
   requestWithDefaults,
@@ -11,12 +13,12 @@ const searchEvents = async (
     const events = fp.flow(
       fp.getOr([], 'body'),
       fp.filter((event) => selectedEvent.id !== event.id),
-      fp.slice(0, 50)
+      fp.slice(0, EVENTS_RETURN_LIMIT)
     )(
       await requestWithDefaults({
         url: `${options.url}/events/index/searchall:${fp.toLower(
           term
-        )}/sort:info/direction:desc`,
+        )}/sort:info/direction:desc/limit:${EVENTS_RETURN_LIMIT}`,
         method: 'GET',
         headers: {
           Authorization: options.apiKey,
